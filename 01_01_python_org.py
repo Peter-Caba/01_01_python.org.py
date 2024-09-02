b4fdc876b2309b0460cf9ecd852133e38616008c
Peter Caba <petercaba902@gmail.com>
2024-09-02 19:59:51 -0400

    Add files via upload

---
+++ 01_01_python_org.py
@@ -0,0 +1,8 @@
+ 1.1 Python.org
+ ...
+python was something that I've never experinced before with 
+new techonlogy I would want to expand what I'm capable of 100%
+Moving on I want to see the how to become a better person with the 
+skills I'm going to aquire when I'm done with this year having fun 
+with new friends and teachers hoping to have a great experince in 
+Albright college
