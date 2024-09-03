d44c88198e5d47284ba1517e3ffcafcec58dc46c
Peter Caba <petercaba902@gmail.com>
2024-09-02 20:01:11 -0400

    Update 01_01_python_org.py

--- 01_01_python_org.py
+++ 01_01_python_org.py
@@ -6,3 +6,4 @@ Moving on I want to see the how to become a better person with the
 skills I'm going to aquire when I'm done with this year having fun 
 with new friends and teachers hoping to have a great experince in 
 Albright college
+ ...
